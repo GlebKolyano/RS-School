6be77960e03c9350fd1e8290fd75aa069863820c
GlebKolyano <88821881+GlebKolyano@users.noreply.github.com>
2022-04-04 13:06:14 +0300

    fix: fix wrong path for pet-service

--- shelter/services/pets.service.js
+++ shelter/services/pets.service.js
@@ -2,14 +2,14 @@ export default class PetsService {
   constructor() {}
   
   async getPetsForSlider() {
-    let pets = await fetch("/assets/our-pets.json")
+    let pets = await fetch("/glebkolyano-JSFE2022Q1/shelter/assets/our-pets.json")
 
     return pets
   }
 
   async getPetsForPagination() {
 
-    let pets = await fetch("/assets/our-pets.json").then(data => data.json())
+    let pets = await fetch("/glebkolyano-JSFE2022Q1/shelter/assets/our-pets.json").then(data => data.json())
     
 
     return [...pets.sort(() => Math.random() - 0.5), ...pets.sort(() => Math.random() - 0.5), ...pets.sort(() => Math.random() - 0.5), ...pets.sort(() => Math.random() - 0.5), ...pets.sort(() => Math.random() - 0.5), ...pets.sort(() => Math.random() - 0.5)]
