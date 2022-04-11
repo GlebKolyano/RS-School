a24590bf13c752b5862ac5d5bce0094b762d9132
GlebKolyano <glebkolyano38@gmail.com>
2022-04-11 09:59:03 +0300

    fix: fix wrong menu display

--- shelter/services/help.service.js
+++ shelter/services/help.service.js
@@ -3,7 +3,7 @@ export default class HelpService {
 
   async getHelpBlocks() {
 
-    let help = await fetch("/glebkolyano-JSFE2022Q1/shelter/assets/our-help.json")
+    let help = await fetch("/assets/our-help.json")
 
     // /glebkolyano-JSFE2022Q1/shelter/assets/our-help.json
     
