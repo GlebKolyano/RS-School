f2d4813a7e485a800ed241c1f44bf6a43d46d31d
GlebKolyano <88821881+GlebKolyano@users.noreply.github.com>
2022-04-15 11:12:48 +0300

    fix: fix bug with burger menu when click links

--- shelter/plugins/burger.js
+++ shelter/plugins/burger.js
@@ -1,10 +1,9 @@
 export default function burgerMenu() {
   let header__burger = document.querySelector(".header__burger")
-let headerNav = document.querySelector(".header__nav")
-
+  let headerNav = document.querySelector(".header__nav")
+  let head = document.body.querySelector(".header.pets-page")
 
 header__burger.addEventListener("click", (e) => {
-  let head = document.body.querySelector(".header.pets-page")
   if (e.target.classList.contains("activeBurger")) {
     e.target.classList.remove("activeBurger")
     headerNav.style.right = "-320px"
@@ -22,6 +21,7 @@ document.body.addEventListener("click", (e) => {
   if (e.target.classList.contains("header") || e.target.classList.contains("container") || e.target.classList.contains("header__nav") || e.target.classList.contains("nav__link") ) {
     header__burger.classList.remove("activeBurger")
     headerNav.style.right = "-320px"
+    head.style.overflow = "hidden"
   }  
 })
 
