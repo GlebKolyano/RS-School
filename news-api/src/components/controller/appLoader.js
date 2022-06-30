22d570fc2169b865e8420e40bf616ef19b2737a6
GlebKolyano <glebkolyano38@gmail.com>
2022-06-30 09:13:13 +0300

    feat: add api-key

--- news-api/src/components/controller/appLoader.js
+++ news-api/src/components/controller/appLoader.js
@@ -3,7 +3,7 @@ import Loader from './loader';
 class AppLoader extends Loader {
     constructor() {
         super('https://newsapi.org/v2/', {
-            apiKey: '', // получите свой ключ https://newsapi.org/
+            apiKey: '04b52a49a5a94d0380df3ab8189a8bae',
         });
     }
 }
