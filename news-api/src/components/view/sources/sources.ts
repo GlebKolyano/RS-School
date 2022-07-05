160696c3bb95b9911f4f125807ca2fa949fd29fd
GlebKolyano <glebkolyano38@gmail.com>
2022-07-05 09:53:32 +0300

    refactor: create constants for elements in sources.ts

--- news-api/src/components/view/sources/sources.ts
+++ news-api/src/components/view/sources/sources.ts
@@ -1,9 +1,9 @@
+import { typesOfSelects } from '../../../modules/constants';
 import { INewsInfo } from '../../../modules/types';
-import { isNotNullElement } from '../../../services/Addtional.service';
 import './sources.css';
 
 class Sources {
-    public draw(data: Readonly<INewsInfo[]>): void {
+    public draw(data: Readonly<INewsInfo[]>): void | never {
         const sources = document.querySelector('.sources') as HTMLElement;
         const sourcesErrorTemp = document.querySelector('#sources__error') as HTMLTemplateElement;
         const sourcesErrorTempClone = sourcesErrorTemp.content.cloneNode(true) as HTMLElement;
@@ -20,12 +20,14 @@ class Sources {
         data.forEach((item: Readonly<INewsInfo>) => {
             const sourceClone = sourceItemTemp.content.cloneNode(true) as HTMLElement;
 
-            if (isNotNullElement(sourceClone, '.source__item-name')) {
-                (sourceClone.querySelector('.source__item-name') as HTMLElement).textContent = item.name;
+            const sourceItemName: HTMLElement | null = sourceClone.querySelector('.source__item-name');
+            if (sourceItemName) {
+                sourceItemName.textContent = item.name;
             } else throw new Error('source__item-name is not found!');
 
-            if (isNotNullElement(sourceClone, '.source__item')) {
-                (sourceClone.querySelector('.source__item') as HTMLElement).setAttribute('data-source-id', item.id);
+            const sourceItem: HTMLElement | null = sourceClone.querySelector('.source__item');
+            if (sourceItem) {
+                sourceItem.setAttribute('data-source-id', item.id);
             } else throw new Error('source__item is not found!');
 
             fragment.append(sourceClone);
@@ -35,9 +37,8 @@ class Sources {
         sources.append(fragment);
     }
 
-    public drawSelects(data: Readonly<INewsInfo[]>): void {
+    public drawSelects(data: Readonly<INewsInfo[]>): void | never {
         if (!data.length) throw Error('Array is empty!');
-        const typesOfSelects: string[] = ['category', 'country', 'language'];
 
         typesOfSelects.forEach((type) => {
             const optionsOfSelect: Set<string> = new Set();
