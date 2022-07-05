a7b0e49c4446d49319824d756a065c5e2d8286f9
GlebKolyano <glebkolyano38@gmail.com>
2022-07-05 09:38:37 +0300

    refactor: create constants for elements in news.ts

--- news-api/src/components/view/news/news.ts
+++ news-api/src/components/view/news/news.ts
@@ -1,10 +1,8 @@
 import { IArticleInfo } from '../../../modules/types';
-import { isNotNullElement } from '../../../services/Addtional.service';
-
 import './news.css';
 
 class News {
-    public draw(data: IArticleInfo[]): void {
+    public draw(data: IArticleInfo[]): void | never {
         if (!data.length) throw Error('Array is empty!');
 
         const news: IArticleInfo[] = data.length >= 10 ? data.filter((_item, idx) => idx < 10) : data;
@@ -15,50 +13,54 @@ class News {
             const newsClone = newsItemTemp.content.cloneNode(true) as HTMLElement;
 
             if (idx % 2) {
-                if (isNotNullElement(newsClone, '.news__item')) {
-                    (newsClone.querySelector('.news__item') as HTMLElement).classList.add('alt');
+                const newsItem: HTMLElement | null = newsClone.querySelector('.news__item');
+                if (newsItem) {
+                    newsItem.classList.add('alt');
                 } else throw new Error('news__item is not found!');
             }
-            if (isNotNullElement(newsClone, '.news__meta-photo')) {
-                (newsClone.querySelector('.news__meta-photo') as HTMLElement).style.backgroundImage = `url(${
+
+            const newsMetaPhoto: HTMLElement | null = newsClone.querySelector('.news__meta-photo');
+            if (newsMetaPhoto) {
+                newsMetaPhoto.style.backgroundImage = `url(${
                     item.urlToImage || require('../../../assets/images/news_placeholder.jpg')
                 })`;
             } else throw new Error('news__meta-photo is not found!');
 
-            if (isNotNullElement(newsClone, '.news__meta-author')) {
-                (newsClone.querySelector('.news__meta-author') as HTMLElement).textContent =
-                    item.author || item.source.name;
+            const newsMetaAuthor: HTMLElement | null = newsClone.querySelector('.news__meta-author');
+            if (newsMetaAuthor) {
+                newsMetaAuthor.textContent = item.author || item.source.name;
             } else throw new Error('news__meta-author is not found!');
 
-            if (isNotNullElement(newsClone, '.news__meta-date')) {
-                (newsClone.querySelector('.news__meta-date') as HTMLElement).textContent = item.publishedAt
-                    .slice(0, 10)
-                    .split('-')
-                    .reverse()
-                    .join('-');
+            const newsMetaDate: HTMLElement | null = newsClone.querySelector('.news__meta-date');
+            if (newsMetaDate) {
+                newsMetaDate.textContent = item.publishedAt.slice(0, 10).split('-').reverse().join('-');
             } else throw new Error('news__meta-date is not found!');
 
-            if (isNotNullElement(newsClone, '.news__meta-photo')) {
-                (newsClone.querySelector('.news__description-title') as HTMLElement).textContent = item.title;
+            const newsDescriptionTitle: HTMLElement | null = newsClone.querySelector('.news__description-title');
+            if (newsDescriptionTitle) {
+                newsDescriptionTitle.textContent = item.title;
             } else throw new Error('news__meta-photo is not found!');
 
-            if (isNotNullElement(newsClone, '.news__description-source')) {
-                (newsClone.querySelector('.news__description-source') as HTMLElement).textContent = item.source.name;
+            const newsDescriptionSource: HTMLElement | null = newsClone.querySelector('.news__description-source');
+            if (newsDescriptionSource) {
+                newsDescriptionSource.textContent = item.source.name;
             } else throw new Error('news__description-sourceis not found!');
 
-            if (isNotNullElement(newsClone, '.news__description-content')) {
-                (newsClone.querySelector('.news__description-content') as HTMLElement).textContent = item.description;
+            const newsDescriptionContent: HTMLElement | null = newsClone.querySelector('.news__description-content');
+            if (newsDescriptionContent) {
+                newsDescriptionContent.textContent = item.description;
             } else throw new Error('news__description-content is not found!');
 
-            if (isNotNullElement(newsClone, '.news__read-more a')) {
-                (newsClone.querySelector('.news__read-more a') as HTMLElement).setAttribute('href', item.url);
+            const newsReadMore: HTMLElement | null = newsClone.querySelector('.news__read-more a');
+            if (newsReadMore) {
+                newsReadMore.setAttribute('href', item.url);
             } else throw new Error('news__read-more a is not found!');
 
             fragment.append(newsClone);
         });
 
-        if (document.querySelector('.news') !== null) {
-            const newsElement = document.querySelector('.news') as HTMLElement;
+        const newsElement: HTMLElement | null = document.querySelector('.news');
+        if (newsElement) {
             newsElement.innerHTML = '';
             newsElement.appendChild(fragment);
         } else throw new Error('news is not found!');
