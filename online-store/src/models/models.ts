aa66b30226515c4042a32c0cea889cb49d592bfd
GlebKolyano <glebkolyano38@gmail.com>
2022-07-11 17:40:55 +0300

    feat: implement interfaces for data

---
+++ online-store/src/models/models.ts
@@ -0,0 +1,16 @@
+export interface IData {
+  bicycles: IBicycle[];
+}
+
+export interface IBicycle {
+  name: string;
+  quantity: number;
+  type: string;
+  brand: string;
+  speeds: number;
+  color: string;
+  weight: number;
+  isPopular: boolean;
+  inBasket: boolean;
+  image: string;
+}
