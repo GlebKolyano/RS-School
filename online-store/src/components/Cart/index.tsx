713b423acd0984293444b668ddb2109dd216504f
GlebKolyano <glebkolyano38@gmail.com>
2022-07-13 17:53:54 +0300

    feat: add some css styles for header

--- online-store/src/components/Cart/index.tsx
+++ online-store/src/components/Cart/index.tsx
@@ -1,7 +1,16 @@
 import React from 'react';
+import { FaShoppingBag } from 'react-icons/fa';
+import './style.css';
 
 function Cart() {
-  return <div>Cart</div>;
+  return (
+    <div className="cart">
+      <div className="cart__wrapper">
+        <FaShoppingBag className="cart__icon" />
+        <span className="cart__counter">20</span>
+      </div>
+    </div>
+  );
 }
 
 export default Cart;
--- online-store/src/components/Header/index.tsx
+++ online-store/src/components/Header/index.tsx
@@ -1,8 +1,17 @@
 import React from 'react';
+import { FaBicycle } from 'react-icons/fa';
+import Cart from '../Cart';
 import './style.css';
 
 function Header() {
-  return <div className="header">header</div>;
+  return (
+    <div className="header">
+      <a href="./">
+        <FaBicycle className="header__logo" />
+      </a>
+      <Cart />
+    </div>
+  );
 }
 
 export default Header;
