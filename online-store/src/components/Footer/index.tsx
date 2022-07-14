f29a11c2a53f5707726fe41f89d6ee5024358f89
GlebKolyano <glebkolyano38@gmail.com>
2022-07-14 20:32:36 +0300

    feat: add content for footer

--- online-store/src/components/Footer/index.tsx
+++ online-store/src/components/Footer/index.tsx
@@ -2,7 +2,25 @@ import React from 'react';
 import './style.css';
 
 function Footer() {
-  return <div className="footer">footer</div>;
+  return (
+    <div className="footer">
+      <div className="footer__info">
+        <a href="https://github.com/GlebKolyano" target="_blank" rel="noreferrer">
+          Gleb Kolyano
+        </a>
+        <a href="https://rs.school/js/" target="_blank" rel="noreferrer">
+          <img
+            className="footer__logo"
+            src="https://rs.school/images/rs_school_js.svg"
+            alt="logo-rs-school"
+          />
+        </a>
+        <a href="https://rs.school/js/" target="_blank" rel="noreferrer">
+          RS School, 2022
+        </a>
+      </div>
+    </div>
+  );
 }
 
 export default Footer;
