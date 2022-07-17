ac16f5c25c333ef56ffe430d276da6011779b9a6
GlebKolyano <glebkolyano38@gmail.com>
2022-07-17 21:46:18 +0300

    feat: add tests for filter-by-search component

--- online-store/src/components/Filters/FilterBySearch/index.tsx
+++ online-store/src/components/Filters/FilterBySearch/index.tsx
@@ -8,29 +8,27 @@ function FilterBySearch() {
   const { searchValue } = useAppSelector((state) => state.searchReducer);
 
   return (
-    <div>
-      <div className="input-field search">
-        <label htmlFor="first_name">
-          Поиск товаров:
-          <input
-            placeholder="Поиск"
-            id="first_name"
-            type="text"
-            className="search__input validate"
-            onChange={(e) => dispatch(setSearch(e.target.value))}
-            value={searchValue}
-            autoFocus
-            autoComplete="off"
-          />
-          <button
-            type="button"
-            className={searchValue.length ? 'search__clear active' : 'search__clear'}
-            onClick={() => dispatch(setSearch(''))}
-          >
-            <i className="material-icons">clear</i>
-          </button>
-        </label>
-      </div>
+    <div className="input-field search" data-testid="filter-by-search">
+      <label htmlFor="first_name">
+        Поиск товаров:
+        <input
+          placeholder="Поиск"
+          id="first_name"
+          type="text"
+          className="search__input validate"
+          onChange={(e) => dispatch(setSearch(e.target.value))}
+          value={searchValue}
+          autoFocus
+          autoComplete="off"
+        />
+        <button
+          type="button"
+          className={searchValue.length ? 'search__clear active' : 'search__clear'}
+          onClick={() => dispatch(setSearch(''))}
+        >
+          <i className="material-icons">clear</i>
+        </button>
+      </label>
     </div>
   );
 }
